160bae11cb1eaedabd7ba3c95795f3500475f42b
kim nam <nam.kim@invite-corp.com>
2015-12-03 21:36:58 +0900

    utf

--- src/instag/wsgi.py
+++ src/instag/wsgi.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 WSGI config for instag project.
 
@@ -10,19 +11,16 @@ import os
 import site
 import sys
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "instag.settings.production")
-
-
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
 # virtualenvのパッケージパス
 site.addsitedir("/home/ec2-user/.virtualenvs/instag/lib/python2.7/site-packages")
-
 sys.path.append('/var/www/instag')
 sys.path.append('/var/www/instag/src')
 sys.path.append('/var/www/instag/src/instag')
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "instag.settings.production")
 
 
 # Wrap werkzeug debugger if DEBUG is on
