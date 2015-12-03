3713d5f08d0c6028c702688ec1cf01af789a44ec
kim nam <nam.kim@invite-corp.com>
2015-12-03 14:35:04 +0900

    add hostname

--- src/instag/settings/__init__.py
+++ src/instag/settings/__init__.py
@@ -4,5 +4,7 @@ from .hostname import HOSTNAME
 
 if HOSTNAME.startswith('kin-no-macbook-pro.local'):
     from .development import *
+elif HOSTNAME.startswith('ip-172-31-22-249'): #本番
+    from .production import *
 elif HOSTNAME.startswith('followkr01.cafe24.com'): #本番
     from .production import *
