a279f586f176507c5d5f21c3c9533b3923e9bdee
kim nam <nam.kim@invite-corp.com>
2015-12-05 15:41:46 +0900

    test

--- src/instag/wsgi.py
+++ src/instag/wsgi.py
@@ -21,13 +21,13 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 import os
 import site
 import sys
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
-# site.addsitedir("/home/ec2-user/.virtualenvs/instag/lib/python2.7/site-packages")
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+site.addsitedir("/home/ec2-user/.virtualenvs/instag/lib/python2.7/site-packages")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "instag.settings.production")
 
-# from django.core.wsgi import get_wsgi_application
-# application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
 
 
 # Wrap werkzeug debugger if DEBUG is on
