775afa49628f900a62a34ee6e62bf683ba5bcf5a
kim nam <nam.kim@invite-corp.com>
2015-12-05 23:24:59 +0900

    test

--- src/instag/wsgi.py
+++ src/instag/wsgi.py
@@ -29,7 +29,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
 # site.addsitedir("/home/ec2-user/.virtualenvs/instag/lib/python2.7/site-packages")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "instag.settings")
 
-import django
+import Django
 # from django import core
 # from django.core.wsgi import get_wsgi_application
 # application = get_wsgi_application()
