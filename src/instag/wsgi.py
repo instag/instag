be4c3c26df1a2d79b9968f54f2da94318c2b3eb9
kim nam <nam.kim@invite-corp.com>
2015-12-05 14:29:31 +0900

    test

--- src/instag/wsgi.py
+++ src/instag/wsgi.py
@@ -16,7 +16,9 @@ site.addsitedir("/home/ec2-user/.virtualenvs/instag/lib/python2.7/site-packages"
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "instag.settings.production")
-print 33
+
+print os.path.dirname(os.path.abspath(__file__))
+print os.path.dirname(os.path.abspath(__file__)) + '/..'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
