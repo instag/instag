bf7dabd556144af55f0bb6fb273e5bcd79219666
kim nam <nam.kim@invite-corp.com>
2015-12-05 14:26:03 +0900

    wsgi

--- src/instag/wsgi.py
+++ src/instag/wsgi.py
@@ -23,6 +23,8 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../..')
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "instag.settings.production")
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
