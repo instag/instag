85c4c0e03448f832c7c0b1ed597b2b1a5f446c38
kim nam <nam.kim@invite-corp.com>
2015-12-06 00:29:48 +0900

    test1

--- src/instag/settings/__init__.py
+++ src/instag/settings/__init__.py
@@ -2,9 +2,13 @@
 import os
 from .hostname import HOSTNAME
 
+print 555
 if HOSTNAME.startswith('kin-no-macbook-pro.local'):
+    print 111
     from .development import *
 elif HOSTNAME.startswith('ip-172-31-22-249'): #本番
+    print 222
     from .production import *
 elif HOSTNAME.startswith('followkr01.cafe24.com'): #本番
+    print 333
     from .production import *
--- src/instag/wsgi.py
+++ src/instag/wsgi.py
@@ -15,53 +15,34 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 # application = get_wsgi_application()
 
 
-# import site
-# site.addsitedir('/home/ec2-user/.virtualenvs/instag/lib/python2.7/site-packages')
-# import os
-# import sys
-# 
-# paths = (
-#    os.path.abspath(os.path.join(os.path.dirname(__file__), '../instag')),
-#    os.path.abspath(os.path.join(os.path.dirname(__file__), '../')),
-#    )
-# 
-# for path in paths:
-#     print path
-#     sys.path.insert(0,path)
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'instag.settings.production'
-# 
-# from django.core.wsgi import get_wsgi_application
-# application = get_wsgi_application()
-# from django.conf import settings
-# if settings.DEBUG:
-#     try:
-#         import django.views.debug
-#         import six
-#         from werkzeug.debug import DebuggedApplication
-# 
-#         def null_technical_500_response(request, exc_type, exc_value, tb):
-#             six.reraise(exc_type, exc_value, tb)
-# 
-#         django.views.debug.technical_500_response = null_technical_500_response
-#         application = DebuggedApplication(application, evalex=True)
-#     except ImportError:
-#         pass
-    
-    
-    
-import os
-import sys
 import site
 site.addsitedir('/home/ec2-user/.virtualenvs/instag/lib/python2.7/site-packages')
+import os
+import sys
+ 
 
-path = '/var/www/instag'
-if path not in sys.path:
-    sys.path.append(path)
+sys.path.append('/var/www/instag')
+sys.path.append('/var/www/instag/src')
+sys.path.append('/var/www/instag/src/instag')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apple.settings")
 
-sys.stdout = sys.stderr
-print sys.path
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'instag.settings.production'
-
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+os.environ['DJANGO_SETTINGS_MODULE'] = 'instag.settings'
+ 
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+from django.conf import settings
+if settings.DEBUG:
+    try:
+        import django.views.debug
+        import six
+        from werkzeug.debug import DebuggedApplication
+ 
+        def null_technical_500_response(request, exc_type, exc_value, tb):
+            six.reraise(exc_type, exc_value, tb)
+ 
+        django.views.debug.technical_500_response = null_technical_500_response
+        application = DebuggedApplication(application, evalex=True)
+    except ImportError:
+        pass
+    
+    
