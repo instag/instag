d4df65b5951bbeee7e47acb0db0bde5de511914a
kim nam <nam.kim@invite-corp.com>
2015-12-05 22:33:44 +0900

    test

--- src/instag/wsgi.py
+++ src/instag/wsgi.py
@@ -30,6 +30,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
 
 print os.path.abspath(__file__)
+print sys.path
 
 
 
