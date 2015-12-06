19f409d50f583868a9e387d74efb1da51c32762b
kim nam <nam.kim@invite-corp.com>
2015-12-06 13:49:42 +0900

    test

--- src/instag/wsgi.py
+++ src/instag/wsgi.py
@@ -9,11 +9,28 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 
-print 1111
+
+# import os
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "instag.settings.production")
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
+
+
+
 import os
+
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "instag.settings.production")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings_dev")
+
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+
+from dj_static import Cling
+application = Cling(get_wsgi_application())
 
 
 
