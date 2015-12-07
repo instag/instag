0f4fad4cf90f979837917ea1047287945f6538ee
kim nam <nam.kim@invite-corp.com>
2015-12-07 23:27:54 +0900

    wsgi

--- src/instag/wsgi.py
+++ src/instag/wsgi.py
@@ -10,36 +10,35 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 
 
 
-# import os
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "instag.settings.production")
-# from django.core.wsgi import get_wsgi_application
-# application = get_wsgi_application()
-
-
-
-
-
-
-
-import site
-# site.addsitedir('/var/www/instag/instag/lib/python2.7/site-packages')
-site.addsitedir('/home/ec2-user/.virtualenvs/tag/lib/python2.7/site-packages')
-
- 
 import os
-import sys
- 
-paths = (
-   os.path.abspath(os.path.join(os.path.dirname(__file__), '../instag')),
-   os.path.abspath(os.path.join(os.path.dirname(__file__), '../')),
-   )
-for path in paths:
-   sys.path.insert(0,path)
- 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'instag.settings.production'
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "instag.settings.production")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+
+
+
+
+
+
+# import site
+# # site.addsitedir('/var/www/instag/instag/lib/python2.7/site-packages')
+# site.addsitedir('/home/ec2-user/.virtualenvs/tag/lib/python2.7/site-packages')
+#  
+# import os
+# import sys
+#  
+# paths = (
+#    os.path.abspath(os.path.join(os.path.dirname(__file__), '../instag')),
+#    os.path.abspath(os.path.join(os.path.dirname(__file__), '../')),
+#    )
+# for path in paths:
+#    sys.path.insert(0,path)
+#  
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'instag.settings.production'
+# 
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
 
 
 
