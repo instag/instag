9bd5e81137b80bc6671759df465ba529feae1267
kim nam <nam.kim@invite-corp.com>
2015-12-07 23:31:00 +0900

    test

--- src/instag/wsgi.py
+++ src/instag/wsgi.py
@@ -9,18 +9,10 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 
-import site
-site.addsitedir('/home/ec2-user/.virtualenvs/tag/lib/python2.7/site-packages')
-
-
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "instag.settings.production")
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-
-
-
-
+# import os
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "instag.settings.production")
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
 
 
 
@@ -46,19 +38,20 @@ application = get_wsgi_application()
 
 
 
-# import site
-# site.addsitedir('/var/www/instag/instag/lib/python2.7/site-packages')
-# import os
-# import sys
-#   
-# sys.path.append('/var/www/instag')
-# sys.path.append('/var/www/instag/src')
-# # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "instag.settings.production")
-#   
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'instag.settings.production'
-#    
-# from django.core.wsgi import get_wsgi_application
-# application = get_wsgi_application()
+import site
+
+site.addsitedir('/home/ec2-user/.virtualenvs/tag/lib/python2.7/site-packages')
+import os
+import sys
+   
+sys.path.append('/var/www/instag')
+sys.path.append('/var/www/instag/src')
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "instag.settings.production")
+   
+os.environ['DJANGO_SETTINGS_MODULE'] = 'instag.settings.production'
+    
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
 
 
 
