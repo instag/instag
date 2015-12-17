c7f0328e3da90471f2445ed0c7ba951fb8049dba
kim nam <nam.kim@invite-corp.com>
2015-12-17 23:58:02 +0900

    instagram insert api

--- src/common/management/commands/insert_user_img_api.py
+++ src/common/management/commands/insert_user_img_api.py
@@ -3,32 +3,48 @@ from django.core.management.base import BaseCommand
 import datetime
 import hashlib
 from instagram_url.models import InstagramPlayer
-from instagram.client import InstagramAPI
+import hmac
+from hashlib import sha256
+import urllib
+import urlparse
+import bottle
+import beaker.middleware
+from bottle import route, redirect, post, run, request, hook
+from instagram import client, subscriptions
 
-# from bootcamp.twits.models import Twit
+bottle.debug(True)
+
+session_opts = {
+    'session.type': 'file',
+    'session.data_dir': './session/',
+    'session.auto': True,
+}
 
 INSTAGRAM_CLIENT_ID = '3dc77d748ec9434fba8d92569824b5ea'
 INSTAGRAM_CLIENT_SECRET = '44dafb59c4d94095a0a326022d7e82c1'
+
+
+app = beaker.middleware.SessionMiddleware(bottle.app(), session_opts)
+
+CONFIG = {
+    'client_id': '3dc77d748ec9434fba8d92569824b5ea',
+    'client_secret': '44dafb59c4d94095a0a326022d7e82c1',
+    'redirect_uri': 'http://localhost:8515/oauth_callback'
+}
+
+unauthenticated_api = client.InstagramAPI(**CONFIG)
 
 class Command(BaseCommand):
     """
     instagramからimgを取得する
     """
+
     def handle(self, *args, **options):
+        api = client.InstagramAPI(access_token='1180381936.3dc77d7.b5311ed9c8db49fda33aad54f6a05dea', client_secret=CONFIG['client_secret'])
+        recent_media, next = api.user_recent_media()
+        print recent_media
         
-            instagram = InstagramPlayer.objects.all()[0]
-            print type(instagram)
-            
-            print instagram.oauth_token
-            print instagram.user_id
-            api = InstagramAPI(access_token=instagram.oauth_token)
-            media, discard = api.user_recent_media(user_id=instagram.user_id, count=24)
-#             try:
-#             except InstagramAPIError as e:
-#                 logger.error(e)
-#                 return {"media": []}
-#             return {"media": media}
-#         except IndexError:
-#             return {"media": []}
-#         
+    
+        
+        
         
