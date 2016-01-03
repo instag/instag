5b55f88b356d40f0399f5e8e3a420507423414a0
kim nam <nam.kim@invite-corp.com>
2016-01-03 23:48:26 +0900

    paging

--- src/instag/views.py
+++ src/instag/views.py
@@ -1,29 +1,20 @@
-from django.views import generic
-from instagram import client, subscriptions
-from .hostname import HOSTNAME
 from common import template_text as T
+from django.core.paginator import EmptyPage
+from django.core.paginator import PageNotAnInteger
+from django.core.paginator import Paginator
+from django.views import generic
+from instagram import client
 from instagram_url.models import InstagramPlayer, InstagramPlayerMedia
 from shop.models import Shop
-
 
 CONFIG = T.CONFIG
 
 unauthenticated_api = client.InstagramAPI(**CONFIG)
 
-#     def get(self, request, *args, **kwargs):
-#         user = self.request.user
-#         if "user_form" not in kwargs:
-#             kwargs["user_form"] = forms.UserForm(instance=user)
-#         if "profile_form" not in kwargs:
-#             kwargs["profile_form"] = forms.ProfileForm(instance=user.profile)
-#         return super(EditProfile, self).get(request, *args, **kwargs)
-# 
-#     def post(self, request, *args, **kwargs):
-
-
-
 class HomePage(generic.TemplateView):
     template_name = "home.html"
+    paginate_by = 20
+
 
     def get(self, request, *args, **kwargs):
         # url = unauthenticated_api.get_authorize_url(scope=["likes","comments"])
@@ -34,9 +25,20 @@ class HomePage(generic.TemplateView):
         if instagram_player:
             shop = Shop.objects.get_or_create(user=instagram_player)
             insta_user_media = InstagramPlayerMedia.get_player_media_list(instagram_player)
+            paginator = Paginator(insta_user_media, self.paginate_by)
+            page = self.request.GET.get('page')
+
+            try:
+                page_obj = paginator.page(page)
+            except PageNotAnInteger:
+                page_obj = paginator.page(1)
+            except EmptyPage:
+                page_obj = paginator.page(paginator.num_pages)
+
+            kwargs["page_obj"] = page_obj
             kwargs["shop"] = shop[0]
             kwargs["profile_picture"] = instagram_player.profile_picture
-            kwargs["media"] = insta_user_media
+            kwargs["media"] = page_obj
         else :
             kwargs["media"] = InstagramPlayerMedia.get_all()
 
