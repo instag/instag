07cd7280870d656e0d621546aaaae99211534b94
kim nam <nam.kim@invite-corp.com>
2016-03-09 00:45:34 +0900

    바치 에러 수정 try, except 추가

--- src/common/management/commands/insert_tag_img_api.py
+++ src/common/management/commands/insert_tag_img_api.py
@@ -13,19 +13,21 @@ unauthenticated_api = client.InstagramAPI(**CONFIG)
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for i in InstagramPlayer.objects.all():
-            api = client.InstagramAPI(access_token=i.oauth_token, client_secret=CONFIG['client_secret'])
+            try :
+                api = client.InstagramAPI(access_token=i.oauth_token, client_secret=CONFIG['client_secret'])
+                tag_search, next_tag = api.tag_search(u'nail art')
+                tag_recent_media, next = api.tag_recent_media(tag_name=tag_search[0].name)
 
-            tag_search, next_tag = api.tag_search(u'네일아트')
-            tag_recent_media, next = api.tag_recent_media(tag_name=tag_search[0].name)
-
-            for media in tag_recent_media:
-                result, is_new = InstagramPlayerMedia.objects.get_or_create(user=i, media_id=media.id)
-                if is_new:
-                    result.low_resolution_url = media.get_low_resolution_url()
-                    result.standard_resolution_url = media.get_standard_resolution_url()
-                    result.thumbnail_url = media.get_thumbnail_url()
-                    result.media_link = media.link
-                    result.media_type = media.type
-                    result.caption = media.caption.text[1:100]
-                    result.tags = str(media.tags)
-                    result.save()
+                for media in tag_recent_media:
+                    result, is_new = InstagramPlayerMedia.objects.get_or_create(user=i, media_id=media.id)
+                    if is_new:
+                        result.low_resolution_url = media.get_low_resolution_url()
+                        result.standard_resolution_url = media.get_standard_resolution_url()
+                        result.thumbnail_url = media.get_thumbnail_url()
+                        result.media_link = media.link
+                        result.media_type = media.type
+                        result.caption = media.caption.text[1:100]
+                        result.tags = str(media.tags)
+                        result.save()
+            except:
+                print "[ERROR] player_id : %s" % i.id
