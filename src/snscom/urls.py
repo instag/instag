d0952ebcc58020efbb6903adac1eafd35508fd6f
kim nam <nam.kim@invite-corp.com>
2016-06-10 01:02:40 +0900

    api server get 追加

--- src/snscom/urls.py
+++ src/snscom/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
 from . import views
 
-
-print 11
 urlpatterns = [
-    # url(r'^$', views.Snscom.as_view(), name='snscom_self'),
+    url(r'^$', views.Snscom.as_view(), name='snscom_self'),
+    url(r'^profile/$', views.Profile.as_view(), name='profile_self'),
     url(r'^tags/$', views.Snscom.as_view(), name='tags_self'),
-    url(r'^test/test/$', views.Snscom.as_view(), name='tags_self'),
+    url(r'^test/test/$', views.Snscom.as_view(), name='tags_self')
 ]
--- src/snscom/views.py
+++ src/snscom/views.py
@@ -21,8 +21,29 @@ class Snscom(generic.TemplateView):
         response['Access-Control-Allow-Origin'] = '*'
         response['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
 
-        return response
+        return response.GET
+        return response.POST
 
     def post(self, request, *args, **kwargs):
         print "post_snscom"
         return HttpResponse("ok")
+
+class Profile(generic.TemplateView):
+
+    def get(self, request, *args, **kwargs):
+        print "get_profile"
+        print args
+        print kwargs
+        print request.GET
+        print request.body
+
+        response = HttpResponse(json.dumps({'test':'test_value'}), content_type="application/json", status=200)
+        response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+
+        return response
+
+    def post(self, request, *args, **kwargs):
+        print "post_profile"
+        return HttpResponse("ok")
+
