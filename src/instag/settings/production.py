bf8ef7673f25ffcdacf67197d395c91a0f503937
kim nam <nam.kim@invite-corp.com>
2016-10-09 23:57:57 +0900

    test

--- src/instag/settings/production.py
+++ src/instag/settings/production.py
@@ -85,7 +85,7 @@ logging.config.dictConfig(LOGGING)
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
         'LOCATION': 'instag.ksrpmm.cfg.apne1.cache.amazonaws.com:11211'
     }
 }
